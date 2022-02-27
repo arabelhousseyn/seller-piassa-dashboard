f6f6c3382e86d7952285a31f070e1214bc82a07f
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-02-27 14:58:57 +0000

    make some changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
+
+
+Route::get('/',[LoginController::class,'showLoginForm']);
 
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
