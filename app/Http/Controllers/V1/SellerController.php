ec6885bafdd2dc4b31dc1fbd8f927a3a9214d472
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-03-13 10:39:20 +0000

    create function to update the seller phone

--- app/Http/Controllers/V1/SellerController.php
+++ app/Http/Controllers/V1/SellerController.php
@@ -195,4 +195,31 @@ class SellerController extends Controller
             return response($data,422);
         }
     }
+
+    public function updateSellerPhone(Request $request,$seller_phone_id)
+    {
+        try {
+            $seller_phone = SellerPhone::findOrFail($seller_phone_id);
+
+            if($seller_phone->phone == $request->phone)
+            {
+                $seller_phone->update($request->only('name'));
+                return response('',204);
+            }
+
+            $rules = [
+                'phone' => 'digits:10,unique:seller_phones,phone'
+            ];
+
+            $validated = $request->validate($rules);
+
+            $seller_phone->update($validated);
+
+            return response('',204);
+
+        }catch (\Exception $exception)
+        {
+            return response(['message' => 'not found'],404);
+        }
+    }
 }
