6989d895bb2e621f116fa23bc94be848381deae3
elhousseyn arab <50387622+arabelhousseyn@users.noreply.github.com>
2022-03-20 12:37:59 +0000

    create StoreSignRequest

--- app/Http/Controllers/V1/SignController.php
+++ app/Http/Controllers/V1/SignController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSignRequest;
 use Illuminate\Http\Request;
 use App\Models\{Sign};
 class SignController extends Controller
@@ -34,9 +35,13 @@ class SignController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreSignRequest $request)
     {
-        //
+        if($request->validated())
+        {
+            Sign::create($request->validated());
+            return response()->noContent();
+        }
     }
 
     /**
