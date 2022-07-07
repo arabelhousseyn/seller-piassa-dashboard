bac0a2672e0f70aa99fd7fae347c3f3ceacfbbc6
arabelhousseyn <hocine.arab1@hotmail.com>
2022-07-07 09:28:09 +0100

    final push

--- resources/js/pages/DashboardPage.vue
+++ resources/js/pages/DashboardPage.vue
@@ -36,6 +36,7 @@ export default {
                     this.$router.push('/home/requests')
                     notification.close()
                 })
+                window.location.reload()
             }
         },
         playSound()
